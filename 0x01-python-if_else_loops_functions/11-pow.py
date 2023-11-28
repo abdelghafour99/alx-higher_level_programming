6427f8cae959786081c041a8c26a96b6873ea441
abdelghafour_99 <abdelghafour83@live.fr>
2023-11-28 10:31:30 -0800

    0x01-python-if_else_loops_functions commit 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b > 0:
-        for i in range(b):
-            a*=a
-    else:
-        for i in range(b):
-            a/=a
-    return a
+    return (a ** b)
