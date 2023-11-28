3de39f1a852204df6165501f878dde69ff271042
abdelghafour_99 <abdelghafour83@live.fr>
2023-11-28 09:29:42 -0800

    0x01-python-if_else_loops_functions commit 05

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print(i, ", ", end="".format(2))
+for i in range(100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
