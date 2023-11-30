fe9399b8577c6f20db1d1caeade8142ed7284941
abdelghafour_99 <abdelghafour83@live.fr>
2023-11-30 04:58:25 -0800

    0x02-python-import_modules commit 0707

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import sys
 import hidden_4 as hid
 
 if __name__ != "__main__":
-    exit()
-
-for ar in dir(hid):
-    if ar[0:2] != "__":
-        print(ar)
+    for ar in dir(hid):
+        if ar[0:2] != "__":
+            print(ar)
