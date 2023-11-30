b6d911ae815bcb08d35ca09fa9e86cd8a6c93c11
abdelghafour_99 <abdelghafour83@live.fr>
2023-11-30 05:10:46 -0800

    0x02-python-import_modules commit 102

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
+
+    if a < b:
+        s = add(a, b)
+        s = add(s, 9)
+        s = add(s, 5)
+        return c
+
+    return sub(a, b)
