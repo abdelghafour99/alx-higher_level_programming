041dce7f18f10e4eb261be2e9c62349c132cd5c0
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-03 03:48:04 -0800

    0x03-python-data_structures commit 0505

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-replace_in_list = __import__('2-replace_in_list').replace_in_list
-
-
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
