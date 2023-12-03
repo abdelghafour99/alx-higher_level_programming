b9eda1d40faeb3a1587f2b8310dfd48be186297d
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-03 03:28:19 -0800

    0x03-python-data_structures commit 04

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,13 @@ replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 
 def new_in_list(my_list, idx, element):
-    new_list = replace_in_list(my_list, idx, new_element)
-    return new_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        new = [0 for i in range(len(my_list))]
+        for i in range(len(my_list)):
+            if idx == i:
+                new[i] = element
+            else:
+                new[i] = my_list[i]
+    return new
