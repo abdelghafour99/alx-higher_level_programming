111f8bd42df7a8a028ff5a9f4f92148e6e35cac0
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-03 04:54:36 -0800

    0x03-python-data_structures commit 07

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    l = [0 ,0]
+    list_tuple = [0, 0]
     for i in range(len(tuple_a)):
-        l[i] += tuple_a[i]
+        list_tuple[i] += tuple_a[i]
 
     for i in range(len(tuple_b)):
-        l[i] += tuple_b[i]
+        list_tuple[i] += tuple_b[i]
 
-    return tuple(l)
+    return tuple(list_tuple)
