c6a3d00408a8901ed5e0134eea2e8653b1a9f543
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-04 11:33:19 -0800

    0x03-python-data_structures commit  06

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for a in matrix:
-            for i in range(len(a)-1):
-                print("{:d} ".format(a[i]), end="")
-            if a:
-                print("{:d}".format(a[-1]))
+    for a in matrix:
+        for i in range(len(a)-1):
+            print("{:d} ".format(a[i]), end="")
+        if a:
+            print("{:d}".format(a[-1]))
+    print()
