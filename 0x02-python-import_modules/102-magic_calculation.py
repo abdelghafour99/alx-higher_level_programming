44edf8a3a1b300d682606104249a05fe2790354d
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-04 12:22:54 -0800

    0x02-python-import_modules commit 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from magic_calculation_102 import add, sub
+
+
 def magic_calculation(a, b):
     if b > a:
         s = add(a, b)
-        for j in range (4, 6):
+        for j in range(4, 6):
             s = add(s, j)
         return (s)
 
