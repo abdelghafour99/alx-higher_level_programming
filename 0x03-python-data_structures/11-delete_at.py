ad34e35be3960ce96044893d8acdd11573f3b270
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-04 10:05:01 -0800

    0x03-python-data_structures commit 1111

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list and idx >= 0 and idx < len(my_list):
-        ne_l = [0 for i in range(len(my_lisit)-1)]
-        b = 0
-        for i in range(len(ne_l)):
-            if i == idx:
-                b = 1
-            else:
-                ne_l[i] = my_list[i + b]
-        return ne_l
-    else:
-        return my_list
+        my_list = my_list[:idx] + my_list[idx + 1:]
+    return my_list
