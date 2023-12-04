5e7ee4bb856d697b3631f24add585198a583779b
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-04 09:35:20 -0800

    0x03-python-data_structures commit 09

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        b = my_list[0]
+        for a in my_list:
+            if a > b:
+                b = a
+        return b
+    else:
+        return None
+        
