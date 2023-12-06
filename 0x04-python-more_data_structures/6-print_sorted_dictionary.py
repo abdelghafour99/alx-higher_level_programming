30a272e1e55c1894c739cfc752fee70da9ce38fb
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-06 15:55:59 -0800

    0x04-python-more_data_structures commit 07

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    my_list = sorted(list(a_dictionary))
-    for a in my_list:
-        print("{} : {}".format(a, a_dictionary[a]))
+    for idx, val in sorted(a_dictionary.items()):
+        print("{}: {}".format(idx, val))
