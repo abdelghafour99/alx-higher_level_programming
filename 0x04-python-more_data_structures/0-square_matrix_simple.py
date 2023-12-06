2faa8a2e13c30616b6558607322870e40b1ece27
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-06 02:35:15 -0800

    0x04-python-more_data_structures commit 01

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    l = [[0 for i in range(len(matrix[0]))] for i in range(len(matrix))]
+    nem = [[0 for i in range(len(matrix[0]))] for i in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            l[i][j] = matrix[i][j] * matrix[i][j]
-    return l
+            nem[i][j] = matrix[i][j] * matrix[i][j]
+    return nem
