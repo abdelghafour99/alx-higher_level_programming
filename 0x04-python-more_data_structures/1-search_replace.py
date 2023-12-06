fbedb7bfb4b56ae096bae4e4f95cf4d1a2b16a54
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-06 10:14:47 -0800

    0x04-python-more_data_structures commit 01

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list and my_list[search]:
+        nel = [replace for i in range(len(my_list))]
         for i in range(len(my_list)):
-            if my_list[i] = search:
-                my_list[i] = replace
-    return my_list
+            if my_list[i] != search:
+                nel[i] = my_list[i]
+    return nel
