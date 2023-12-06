1d04b9693a5022063ff1e9ad20e524895e39c2ea
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-06 15:50:36 -0800

    0x04-python-more_data_structures commit 01

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list and my_list[search]:
-        nel = [replace for i in range(len(my_list))]
-        for i in range(len(my_list)):
-            if my_list[i] != search:
-                nel[i] = my_list[i]
+    nel = [replace if a == search else a for a in my_list]
     return nel
