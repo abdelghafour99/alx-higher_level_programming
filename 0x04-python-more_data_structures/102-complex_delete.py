f270c487302ee3efd457e2bc5db389a79623cf30
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-07 03:02:48 -0800

    0x04-python-more_data_structures commit 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,5 +3,5 @@ def complex_delete(a_dictionary, value):
     for i in list(a_dictionary):
         if a_dictonary[i] == value:
             del a_dictionary[i]
-    return a_dictionary[i]
+    return a_dictionary
 #{k: v if v != value else None for k, v in a_dictionary.items()}
