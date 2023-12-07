6bebae6ec24c48f3fda539d236d92cdc0b816258
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-07 02:22:35 -0800

    0x04-python-more_data_structures commit 1212

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,17 @@
 def roman_to_int(roman_string):
     my_list = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     S = 0
-    for i in range(len(roman_string)):
-        if roman_string[i] not in my_list:
-            return 0
-        a = my_list[roman_string[i]]
-        c = 1
-        if i < len(roman_string) - 1:
-            b = my_list[roman_string[i + 1]]
-            if a < b:
-                S -= a
-                c = 0
-        if c:
-            S += a
+    if roman_string:
+        for i in range(len(roman_string)):
+            if roman_string[i] not in my_list:
+                return 0
+            a = my_list[roman_string[i]]
+            c = 1
+            if i < len(roman_string) - 1:
+                b = my_list[roman_string[i + 1]]
+                if a < b:
+                    S -= a
+                    c = 0
+            if c:
+                S += a
     return S
