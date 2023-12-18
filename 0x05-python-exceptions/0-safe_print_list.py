717ca4f1f9a893af080b447a953f443307a46209
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-18 14:24:03 -0800

    0x05-python-exceptions commit 4

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[p], end="")
             p += 1
-        except:
+        except Exception:
             break
     print("")
     return(p)
