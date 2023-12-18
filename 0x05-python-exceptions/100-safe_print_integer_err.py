220d68d7d06968c6182402bb156f379d8147f70e
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-18 14:43:50 -0800

    0x05-python-exceptions commit 100100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError as tr:
+    except ValueError as tr:
         print(f"Exception: {tr}", file=sys.stderr)
         return False
