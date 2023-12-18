b93a5a5a6af0159ca317b44d00cc3c08d331299c
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-18 13:48:34 -0800

    0x05-python-exceptions commit 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[p], end="")
             p += 1
-        except IndexError:
+        except:
             break
     print("")
     return(p)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     p = 0
-    i = 0
-    while p < x:
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             p += 1
-            i += 1
         except (ValueError, TypeError):
-            i += 1
-        except IndexError:
-            break
+            continue
     print("")
     return(p)
