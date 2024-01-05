07673a21e2f36ba1c43ece6e3e166587b05c8c8f
abdelghafour99 <abdelghafour83@live.fr>
2024-01-04 16:18:35 -0800

    0x08-python-more_classes commit 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -19,9 +19,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -31,20 +31,25 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) is not int:
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError("height must be >= 0")
         self.__height = value
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __str__(self):
         total = ""
-        if self.__height == 0 or self.width == 0:
-            return total
         for i in range(self.__height):
             total += ("#" * self.__width)
             if i is not self.__height - 1:
                 total += "\n"
         return total
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.num_instance -= 1
 
     def area(self):
         return self.__width * self.__height
@@ -53,10 +58,3 @@ class Rectangle:
         if self.__width is 0 or self.__height is 0:
             return 0
         return (2 * self.__width) + (2 * self.__height)
-
-    def __repr__(self):
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
-    def __del__(self):
-        print("Bye rectangle...")
-        Rectangle.num_instance -= 1
