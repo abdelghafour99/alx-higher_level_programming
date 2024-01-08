ac61b721353c05d897eef451c41506f27257feed
abdelghafour99 <abdelghafour83@live.fr>
2024-01-08 08:58:25 -0800

    last commit9999

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+lazy_matrix_mul
+"""
+
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function calculates matrix multiplication"""
+    return numpy.matmul(m_a, m_b)
