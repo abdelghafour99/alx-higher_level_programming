77416d355dee66ac10807a767ccfdeddeff4a8ef
abdelghafour99 <abdelghafour83@live.fr>
2024-01-08 09:00:59 -0800

    0x07-python-test_driven_development commit 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+function that adds 2 integers
+"""
+
+
 def add_integer(a, b=98):
+    """the code """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
