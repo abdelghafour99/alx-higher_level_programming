f1de1a59b7a1250331e9a0e2c14337ff26b3a577
abdelghafour99 <abdelghafour83@live.fr>
2024-01-09 04:04:55 -0800

    0x0B-python-input_output commit 0

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """Class"""
+    pass
+    
+    def print_sorted(self):
+        """The function"""
+
+        print(sorted(list(self)))
