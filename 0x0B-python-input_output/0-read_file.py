b74a29cc996aa2f209256f3acbcccdacf8567a8c
abdelghafour99 <abdelghafour83@live.fr>
2024-01-09 04:27:12 -0800

    0x0B-python-input_output commit  1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read, end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that writes a text file (UTF8) and prints it to stdout
+"""
+
+
+def write_file(filename=""):
+    with open(filename, "w", encoding="utf-8") as f:
+        read_data = f.read
+        print(read_data, end="")
+        return(len(read_data))
