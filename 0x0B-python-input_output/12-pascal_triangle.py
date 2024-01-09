1adcc3fb37e1b5a9d4880ffc49eee97943ae264b
abdelghafour99 <abdelghafour83@live.fr>
2024-01-09 09:45:48 -0800

    0x0B-python-input_output commit 120

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+that returns a list of lists of integers
+representing the Pascal’s triangle of n
+"""
+
+
+def pascal_tri(n):
+    """Return the last list of lists"""
+
+    if n == 1:
+        return [1]
+    else:
+        lis = [1 for i in range(n)]
+        for i in range(1, n-1):
+            lis[i] = pascal_tri(n-1)[i-1] + pascal_tri(n-1)[i]
+    return l
+
+
+def pascal_triangle(n):
+    """The code"""
+
+    if type(n) != int:
+        raise TypeError("n should be an integer")
+    if n <= 0:
+        print([])
+    else:
+        for i in range(1, n+1):
+
+            print(pascal_tri(i))
