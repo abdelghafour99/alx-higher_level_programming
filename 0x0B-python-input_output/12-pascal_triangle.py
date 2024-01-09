8eb1be34c67dd06005158f75fe3e217d1fc232bf
abdelghafour99 <abdelghafour83@live.fr>
2024-01-09 13:29:02 -0800

    0x0B-python-input_output commit 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,13 +8,15 @@ representing the Pascal’s triangle of n
 def pascal_tri(n):
     """Return the last list of lists"""
 
-    if n == 1:
-        return [1]
-    else:
-        lis = [1 for i in range(n)]
-        for i in range(1, n-1):
-            lis[i] = pascal_tri(n-1)[i-1] + pascal_tri(n-1)[i]
-    return lis
+    m = 1
+    l1 = [1]
+    while n >= m:
+        l2 = [1 for i in range(m)]
+        for i in range(1, m-1):
+            l2[i] = l1[i-1] + l1[i]
+        l1 = l2
+        m += 1
+    return l1
 
 
 def pascal_triangle(n):
