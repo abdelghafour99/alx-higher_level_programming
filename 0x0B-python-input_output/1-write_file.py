f28bb3c8fa2c21c8175f5d0b039b0afcbcd2cb38
abdelghafour99 <abdelghafour83@live.fr>
2024-01-09 06:22:31 -0800

    0x0B-python-input_output commit 11

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,10 +4,9 @@ function that writes a text file (UTF8) and prints it to stdout
 """
 
 
-def write_file(filename=""):
+def write_file(filename="", text=""):
     """The code"""
 
     with open(filename, "w", encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end="")
-        return(len(read_data))
+        f.write(text)
+        return(len(text))
