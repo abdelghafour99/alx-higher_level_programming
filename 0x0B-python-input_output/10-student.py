355ce891906025a0d45896a23a70bb0532868f7f
abdelghafour99 <abdelghafour83@live.fr>
2024-01-09 14:40:26 -0800

    0x0B-python-input_output commit 1011

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-My class student
-"""
+"""My class Student"""
 
 
 class Student:
-    """The class Student"""
+    """The Class student"""
 
     def __init__(self, first_name, last_name, age):
         """Initializes a new Student"""
-        self.__name = name
-        self.number = number
+
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance:
          - If attrs is a list of strings,
@@ -23,5 +22,5 @@ class Student:
 
         if (type(attrs) == list and
                 all(type(elem) == str for elem in attrs)):
-            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
         return self.__dict__
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-My class student
-"""
+"""My class Student"""
 
 
 class Student:
-    """The class Student"""
+    """The Class student"""
 
     def __init__(self, first_name, last_name, age):
         """Initializes a new Student"""
 
-        self.__name = name
-        self.number = number
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Retrieves a dictionary representation of a Student instance"""
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance:
+        """
 
         return self.__dict__
