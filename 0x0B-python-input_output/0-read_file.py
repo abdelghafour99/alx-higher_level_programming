fd42224796266ecde2fdcdb8e30fb2e3f528b67f
abdelghafour99 <abdelghafour83@live.fr>
2024-01-09 04:15:35 -0800

    0x0B-python-input_output commit 00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ function that reads a text file (UTF8) and prints it to stdout
 
 
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
