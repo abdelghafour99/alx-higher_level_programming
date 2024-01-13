904a7dac49c9a450312480c50f3d55c01a8d5536
abdelghafour99 <abdelghafour83@live.fr>
2024-01-13 02:19:30 -0800

    0x06-python-classes commit 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,14 +18,11 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        """Returns current square area"""
         return self.__size**2
 
     def __init__(self, size=0):
-        """Initialises the data"""
         self.size = size
 
     @property
     def size(self):
-        """Getter method"""
         return self.__size
