0f04c2649dd82f8579bfc50b2da9c298a6fe415d
abdelghafour99 <abdelghafour83@live.fr>
2024-02-14 14:32:15 -0800

    0x0E-SQL_more_queries commit 6

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,12 +3,8 @@
 --  - The user_0d_2 password should be set to user_0d_2_pwd
 --  - If the database hbtn_0d_2 already exists, your script should not fail
 --  - If the user user_0d_2 already exists, your script should not fail
-IF EXISTS(SELECT * from sys.databases WHERE name='hbtn_0d_2')
-BEGIN
-    DROP DATABASE hbtn_0d_2;
-END
 
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
 ALTER USER 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
 GRANT SELECT on hbtn_0d_2 TO 'user_0d_2'@'localhost' WITH GRANT OPTION;;
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,13 +1,4 @@
 -- A script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
-IF EXISTS(SELECT * from sys.databases WHERE name='hbtn_0d_usa') 
-BEGIN 
-    DROP DATABASE hbtn_0d_usa;
-END 
-
-CREATE DATABASE hbtn_0d_usa;
-GO
-
-USE hbtn_0d_usa;
-GO
+CREATE DATABASE IF NOT  EXISTS 'hbtn_0d_usa';
 
 CREATE TABLE states(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
