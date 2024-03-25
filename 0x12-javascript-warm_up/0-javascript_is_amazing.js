a52c5ea5e44156802f1228f36c1978eb1b9391be
abdelghafour99 <abdelghafour83@live.fr>
2024-03-24 19:18:39 -0700

    0x12-javascript-warm_up commit 1

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // this Script Print string “Javascript is amazing”
 
-const myVar = 'Javascript is amazing';
-console.log(myVar);
+const myv = 'JavaScript is amazing';
+console.log(myv);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// this Script Print 3 strings
+
+const myv1 = 'C is fun\n';
+const myv2 = 'Python is cool\n';
+const myv3 = 'JavaScript is amazing';
+console.log(myv1, myv2, myv3);
