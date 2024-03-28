1fd719d4f6c5cf7a3f745b17321fbc49793f7e01
abdelghafour99 <abdelghafour83@live.fr>
2024-03-27 19:35:37 -0700

    0x10-python-network_0 commit 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,8 +3,21 @@
 
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
+    list_len = len(list_of_integers)
+    if list_len == 0:
         return None
-    elif len(list_of_integers) == 1:
-        return list_of_integers[0]
-    return max(list_of_integers[0], find_peak(list_of_integers[1:]))
+    peak = find_peak_index(list_of_integers, 0, list_len - 1)
+    return list_of_integers[peak]
+
+
+""" find_peak_index """
+
+
+def find_peak_index(list_li, list_0, list_ln):
+    if list_0 >= list_ln:
+        return list_0
+    mi = ((list_ln - list_0) // 2) + list_0
+    if list_li[mi] > list_li[mi + 1]:
+        return find_peak_index(list_li, list_0, mi)
+    else:
+        return find_peak_index(list_li, mi + 1, list_ln)
