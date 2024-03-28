7973526fed053dad921e8bd6f45dfee76aa2a80c
abdelghafour99 <abdelghafour83@live.fr>
2024-03-27 18:33:21 -0700

    0x10-python-network_0 commit 0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This bash script takes in a URL, sends a request to that URL
+# and displays size of the body of the response
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
