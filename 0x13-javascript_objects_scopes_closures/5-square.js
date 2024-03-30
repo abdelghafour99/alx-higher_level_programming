c600e660cdcc969735ed8ac1c29187949215c2c4
abdelghafour99 <abdelghafour83@live.fr>
2024-03-29 19:40:27 -0700

    0x13-javascript_objects_scopes_closures commit 55

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // A class Rectangle that defines a rectangle
 module.exports = class Square extends require('./4-rectangle.js') {
-	constructor (size) {
-		super(size, size);
+  constructor (size) {
+    super(size, size);
   }
 };
