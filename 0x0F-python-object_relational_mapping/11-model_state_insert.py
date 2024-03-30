cd3a5b4a66e9348e50d363d74b4efb60fa67e7d4
abdelghafour99 <abdelghafour83@live.fr>
2024-03-29 17:36:04 -0700

    0x0F-python-object_relational_mapping commit 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,11 +8,11 @@ from model_state import State
 
 
 if __name__ == "__main__":
-    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                        .format(sys.argv[1],
-                                sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=eng)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     louisiana = State(name="Louisiana")
