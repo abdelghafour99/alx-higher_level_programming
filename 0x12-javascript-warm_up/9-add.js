5440da1b528b689bcd9459d39173d094ff8a58e6
abdelghafour99 <abdelghafour83@live.fr>
2024-03-29 17:02:01 -0700

    0x11-python-network_1 commit 10

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // this Script Print the sum of tow number
 function add (a, b) {
-  console.log(parseInt(a) + parseInt(b));
+  return parseInt(a) + parseInt(b);
 }
+console.log(add(process.argv[2], process.argv[3]));
