6d8761275bfaea097bf9c9d90bf4f7ff7809295c
abdelghafour99 <abdelghafour83@live.fr>
2024-03-29 17:53:28 -0700

    0x0F-python-object_relational_mapping commit 11

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,9 +14,12 @@ if __name__ == "__main__":
                                    sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+
     states_to_delete = session.query(State).filter(State
                                                    .name.like('%a%')).all()
+
     for state in states_to_delete:
         session.delete(state)
+
     session.commit()
     session.close()
