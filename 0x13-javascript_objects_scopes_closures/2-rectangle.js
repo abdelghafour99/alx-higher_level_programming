51e181898d083814d8d73a617447f96330457471
abdelghafour99 <abdelghafour83@live.fr>
2024-03-29 18:22:14 -0700

    0x13-javascript_objects_scopes_closures commit 6

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// A class Rectangle that defines a rectangle
 module.exports = class Rectangle {
 	constructor (w, h) {
 		if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
